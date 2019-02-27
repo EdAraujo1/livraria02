ed7ecf85a45e272356ccd1803cd67088b043e4a9
Ed Araujo <edarasi1976@gmail.com>
2019-02-26 22:10:10 -0300

    primeiro git

---
+++ src/br/com/bg/livraria02/bean/LivroBean.java
@@ -0,0 +1,13 @@
+package br.com.bg.livraria02.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+	
+	public void gravarLivro() {
+		System.out.println("Gravando Livro!");
+	}
+	
+
+}
